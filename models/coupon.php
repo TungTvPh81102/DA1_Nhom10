43759665b5a7734c2e584bd9a6ac04558d41ee19
TungTvPh81102 <tungtvph46392@fpt.edu.vn>
2024-04-03 00:40:01 +0700

    Fix lỗi mã giảm giá

--- models/coupon.php
+++ models/coupon.php
@@ -3,7 +3,9 @@ if (!function_exists('updateQuantityCoupon')) {
     function updateQuantityCoupon($id, $quantity)
     {
         try {
-            $sql = "UPDATE coupons SET quantity = quantity - :quantity WHERE id = :id";
+            if ($quantity > 0) {
+                $sql = "UPDATE coupons SET quantity = quantity - :quantity WHERE id = :id";
+            }
             $stmt = $GLOBALS['conn']->prepare($sql);
             $stmt->bindParam(':id', $id);
             $stmt->bindParam(':quantity', $quantity);
