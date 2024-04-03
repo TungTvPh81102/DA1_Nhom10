6652113cf5336313013d4094370cea14be35bc36
Setting <minhbyn00000@gmail.com>
2024-04-03 08:21:09 +0700

    Chức năng Banner

--- admin/controllers/BannerController.php
+++ admin/controllers/BannerController.php
@@ -34,3 +34,120 @@ function bannerCreate()
     }
     require_once PATH_VIEW_ADMIN . 'layout/master.php';
 }
+
+function bannerUpdate(){
+    $id = $_GET['id'];
+    $banner = showOne('banners', $id);
+
+    
+    if (!empty($_POST)) {
+        $data = [
+            'heading' => $_POST['heading'] ?? $banner['heading'],
+            'description' => $_POST['description'] ?? $banner['description'],
+            'image' => get_file_upload('image') ?? $banner['image'],
+            'classify' => $_POST['classify'] ?? $banner['classify'],
+            'status' => $_POST['status'] ?? $banner['status'],
+            'created_at' => date('Y-m-d H:i:s')
+        ];
+
+        $imgThumbnail = $data['image'];
+        if (is_array($imgThumbnail)) {
+            $data['image'] = upload_file($imgThumbnail, "uploads/banners/");
+        }
+
+        update('banners', $id, $data);
+        $_SESSION['success'] = 'Thao tác thành công';
+        redirect(BASE_URL_ADMIN . '??action=banner-update&id=' .$id);
+    }
+
+    $title = "Cập nhật Bìa quảng cáo: ";
+    $view = "banner/updateView";
+
+    require_once PATH_VIEW_ADMIN . 'layout/master.php';
+}
+
+function bannerDetail()
+{
+    $id = $_GET['id'];
+    $banner = showOne('banners', $id);
+
+    if (empty($banner)) {
+        e404();
+    }
+
+    $title = 'Chi tiết bìa quảng cáo: ' . $banner['heading'];
+    $view = 'banner/showView';
+    require_once PATH_VIEW_ADMIN . "layout/master.php";
+}
+
+function bannerDelete()
+{
+    $id = $_GET['id'];
+    $banner = showOne('banners', $id);
+
+    if (empty($banner)) {
+        e404();
+    }
+
+    try {
+        $GLOBALS['conn']->beginTransaction();
+
+        deleteRow('banners', $id);
+
+        // die();
+        $GLOBALS['conn']->commit();
+    } catch (Exception $e) {
+
+        $GLOBALS['conn']->rollBack();
+
+        debug($e);
+    }
+
+    if (!empty($banner['image']) && file_exists(PATH_UPLOAD . $banner['image'])) {
+        unlink(PATH_UPLOAD . $banner['image']);
+    }
+
+
+
+    $arrayThumbnails = explode(',', $banner['image']);
+
+    foreach ($arrayThumbnails as $image => $value) {
+        // debug(file_exists(PATH_UPLOAD .  $value));
+
+        unlink($value);
+    }   
+
+    $_SESSION['success'] = 'Xóa bìa quảng cáo thành công';
+    redirect(BASE_URL_ADMIN . "?action=banners-list");
+    exit();
+}
+
+function validateBannerCreate($data)
+{
+    $errors = [];
+
+    if (empty(trim($data['heading']))) {
+        $errors['heading']['required'] = 'Vui lòng nhập tên bìa';
+    } else {
+        if (!preg_match('/^[a-zA-ZÀ-ỹ\s]+$/', $data['heading'])) {
+            $errors['heading']['text'] = 'Tên bìa phải là ký tự';
+        } else if (!checkUniqueheading('Banners', $data['heading'])) {
+            $errors['heading']['unquied'] = 'Tên bìa đã tồn tại';
+        }
+    }
+
+    if ($data['status'] === null) {
+        $errors['status']['required'] = 'Vui lòng chọn trạng thái';
+    } else {
+        if (!in_array($data['status'], [0, 1])) {
+            $errors['status']['status'] = 'Trạng thái phải là Active hoặc Inactive';
+        }
+    }
+
+    if (!empty($errors)) {
+        $_SESSION['errors'] = $errors;
+        $_SESSION['data'] = $data;
+        redirect(BASE_URL_ADMIN . "?action=Banner-create");
+        exit();
+    }
+}
